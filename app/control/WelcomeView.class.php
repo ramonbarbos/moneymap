2b79abd4c2506db0189fdeac6ec72efc70bcbcf2
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-12-13 10:42:10 -0300

    info dashborad

--- app/control/WelcomeView.class.php
+++ app/control/WelcomeView.class.php
@@ -2,6 +2,7 @@
 
 use Adianti\Control\TPage;
 use Adianti\Database\TTransaction;
+use Adianti\Registry\TSession;
 use Adianti\Widget\Container\TPanelGroup;
 use Adianti\Widget\Container\TVBox;
 use Adianti\Widget\Template\THtmlRenderer;
@@ -32,8 +33,9 @@ class WelcomeView extends TPage
 
            
             
-               
-
+            $anoMes = '202312';
+            $cpf = '85778905548';
+            $tp_folha = '1';
         
 
             $html = new THtmlRenderer('app/templates/theme3/system_dashboard.html');
@@ -42,16 +44,20 @@ class WelcomeView extends TPage
             $indicator2 = new THtmlRenderer('app/templates/theme3/info-box.html');
             $indicator3 = new THtmlRenderer('app/templates/theme3/info-box.html');
 
-            $saldoAtual = new Despesa(1);
+            $saldoAtual = Despesa::where('anoMes','=',$anoMes)
+                                ->where('tp_folha','=',$tp_folha)
+                                ->where('cpf','=',$cpf)->first();
+
+            $receita = $saldoAtual->saldo + $saldoAtual->vl_despesa;
 
 
-            $indicator1->enableSection('main', ['title' => 'Folha',    'icon' => 'cube', 'background' => 'orange', 'text' => 'SALDO ATUAL', 'value' => $saldoAtual->saldo]);
+            $indicator1->enableSection('main', ['title' => 'Folha',    'icon' => 'cube', 'background' => 'orange', 'text' => 'SALDO ATUAL', 'value' => 'R$ '.$saldoAtual->saldo]);
 
-          //  $indicator2->enableSection('main', ['title' => 'Estoque',    'icon' => 'box', 'background' => 'blue', 'text' => 'QUANTIDADE DE PRODUTO NO ESTOQUE', 'value' => $estoqueTotal]);
+            $indicator2->enableSection('main', ['title' => 'Despesa',    'icon' => 'box', 'background' => 'blue', 'text' => 'RECEITAS', 'value' => 'R$ '.$receita]);
 
-           // $indicator3->enableSection('main', ['title' => 'Estoque',    'icon' => 'dollar-sign', 'background' => 'green', 'text' => 'CUSTO TOTAL DE PRODUTOS', 'value' =>   'R$ '.$valorTotal]);
+            $indicator3->enableSection('main', ['title' => 'Despesa',    'icon' => 'dollar-sign', 'background' => 'green', 'text' => 'DESPESAS', 'value' =>   'R$ '.$saldoAtual->vl_despesa]);
 
-            $html->enableSection('main', ['indicator1' => $indicator1]);
+            $html->enableSection('main', ['indicator1' => $indicator1,'indicator2' => $indicator2,'indicator3' => $indicator3]);
 
             $container = new TVBox;
             $container->style = 'width: 100%';
